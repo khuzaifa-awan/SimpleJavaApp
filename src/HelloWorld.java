c107418d6340ca5b5fe150cf11e9eb87532886bc
Khuzaifa Awan <sp22-bcs-020@cuiatk.edu.pk>
2025-06-03 16:25:05 +0500

    Test CI/CD working

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello from Jenkins – Test 2 Build Trigger!");
+        System.out.println("Testing CI/CD ");
     }
 }
